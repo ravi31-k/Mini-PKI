d0395e3b111b6558155004b4fa11fc1794e86ecf
Ravindra Malviya <ravindramalviya06@gmail.com>
2025-04-09 23:48:02 +0530

    Many Updation

--- app.py
+++ app.py
@@ -89,5 +89,9 @@ def revoke():
     flash(f"Certificate revoked for {name}.", "danger")
     return redirect(url_for("home"))
 
+# if __name__ == "__main__":
+#     app.run(debug=True) 
+# when PUSHED to Redar, uncomment the following lines
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
